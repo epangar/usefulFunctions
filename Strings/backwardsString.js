d4fc3f84c07494e5485b5cc3109885077231148f
Tanis Pan <e.pan.gar@gmail.com>
2018-06-09 12:14:57 +0200

    backwards string

--- Strings/backwardsString.js
+++ Strings/backwardsString.js
@@ -1,2 +1,12 @@
 //Receives a string and returns it backwards
 
+function backwardsString(str){
+
+  var answer= ""
+
+  for(i=str.length-1;i>=0;i--){
+    answer+=str[i]
+  }
+  
+  return answer;
+}
