08ca5540b415f67972620fa3b5fe6069825dae30
Tanis Pan <e.pan.gar@gmail.com>
2018-07-13 21:04:09 +0200

    .

--- Math/binaryToDecimal.js
+++ Math/binaryToDecimal.js
@@ -7,10 +7,8 @@ function toDecimal(x){
   var l = x.length;
 
   for(i=0;i<l;i++){
-    console.log(x[i],)
+    console.log(x[i])
     answer+=(parseInt(x[i])*Math.pow(2,l-i-1))
   }
   return answer;
 }
-
-//101010=42 => 2⁵+2³+2¹
